c36d253a32c4fbfc76d1d8b0169011ad4b7ea8de
granitosaurus <bernardas.alisauskas@pm.me>
2023-12-21 23:41:35 +0700

    revert api key setting to a proper place holder

--- examples/scrapy/demo/demo/settings.py
+++ examples/scrapy/demo/demo/settings.py
@@ -1,6 +1,6 @@
 SPIDER_MODULES = ['demo.spiders']
 NEWSPIDER_MODULE = 'demo.spiders'
-SCRAPFLY_API_KEY = '2fbb226543e44335adb496d30c3ba92d'
+SCRAPFLY_API_KEY = '__API_KEY__'
 CONCURRENT_REQUESTS = 2
 IMAGES_STORE = "./images"
 # Scrapfly SDK supports the new Scrapy asyncio reactor as well, enable it with:
