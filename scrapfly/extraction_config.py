f13fb45d36678a77ad28c85bb53ead4baac785b1
Mazen Ramadan <mazenramadan00@gmail.com>
2024-11-08 16:20:49 +0200

    add scrape extraction params and rename extraction template options (#25)
    * add scrape extraction params and rename extraction template options
    
    * accept deprecated params
    
    * add scrape extraction examples

--- scrapfly/extraction_config.py
+++ scrapfly/extraction_config.py
@@ -1,4 +1,5 @@
 import json
+import warnings
 from enum import Enum
 from typing import Optional, Dict
 from urllib.parse import quote_plus
@@ -29,8 +30,8 @@ class ExtractionConfig(BaseApiConfig):
     content_type: str
     url: Optional[str] = None
     charset: Optional[str] = None
-    template: Optional[str] = None  # a saved template name
-    ephemeral_template: Optional[Dict]  # ephemeraly declared json template
+    extraction_template: Optional[str] = None  # a saved template name
+    extraction_ephemeral_template: Optional[Dict]  # ephemeraly declared json template
     extraction_prompt: Optional[str] = None
     extraction_model: Optional[str] = None
     is_document_compressed: Optional[bool] = None
@@ -38,29 +39,49 @@ class ExtractionConfig(BaseApiConfig):
     webhook: Optional[str] = None
     raise_on_upstream_error: bool = True
 
+    # deprecated options
+    template: Optional[str] = None
+    ephemeral_template: Optional[Dict] = None
+
     def __init__(
         self,
         body: str,
         content_type: str,
         url: Optional[str] = None,
         charset: Optional[str] = None,
-        template: Optional[str] = None,  # a saved template name
-        ephemeral_template: Optional[Dict] = None,  # ephemeraly declared json template
+        extraction_template: Optional[str] = None,  # a saved template name
+        extraction_ephemeral_template: Optional[Dict] = None,  # ephemeraly declared json template
         extraction_prompt: Optional[str] = None,
         extraction_model: Optional[str] = None,
         is_document_compressed: Optional[bool] = None,
         document_compression_format: Optional[CompressionFormat] = None,
         webhook: Optional[str] = None,
-        raise_on_upstream_error: bool = True
+        raise_on_upstream_error: bool = True,
+
+        # deprecated options
+        template: Optional[str] = None,
+        ephemeral_template: Optional[Dict] = None     
     ):
+        if template:
+            print("WARNGING")
+            warnings.warn(
+                "Deprecation warning: 'template' is deprecated. Use 'extraction_template' instead."
+            )
+            extraction_template = template
+
+        if ephemeral_template:
+            warnings.warn(
+                "Deprecation warning: 'ephemeral_template' is deprecated. Use 'extraction_ephemeral_template' instead."
+            )
+            extraction_ephemeral_template = ephemeral_template
 
         self.key = None
         self.body = body
         self.content_type = content_type
         self.url = url
         self.charset = charset
-        self.template = template
-        self.ephemeral_template = ephemeral_template
+        self.extraction_template = extraction_template
+        self.extraction_ephemeral_template = extraction_ephemeral_template
         self.extraction_prompt = extraction_prompt
         self.extraction_model = extraction_model
         self.is_document_compressed = is_document_compressed
@@ -80,7 +101,7 @@ class ExtractionConfig(BaseApiConfig):
                 else:
                     raise ExtractionConfigError(
                         f'Auto compression for {self.document_compression_format.value} format is not available. You can manually compress to {self.document_compression_format.value} or choose the gzip format for auto compression.'
-                    )                    
+                    )
 
     def to_api_params(self, key: str) -> Dict:
         params = {
@@ -94,15 +115,15 @@ class ExtractionConfig(BaseApiConfig):
         if self.charset:
             params['charset'] = self.charset
 
-        if self.template and self.ephemeral_template:
-            raise ExtractionConfigError('You cannot pass both parameters template and ephemeral_template. You must choose')
+        if self.extraction_template and self.extraction_ephemeral_template:
+            raise ExtractionConfigError('You cannot pass both parameters extraction_template and extraction_ephemeral_template. You must choose')
 
-        if self.template:
-            params['extraction_template'] = self.template
+        if self.extraction_template:
+            params['extraction_template'] = self.extraction_template
 
-        if self.ephemeral_template:
-            self.ephemeral_template = json.dumps(self.ephemeral_template)
-            params['extraction_template'] = 'ephemeral:' + urlsafe_b64encode(self.ephemeral_template.encode('utf-8')).decode('utf-8')
+        if self.extraction_ephemeral_template:
+            self.extraction_ephemeral_template = json.dumps(self.extraction_ephemeral_template)
+            params['extraction_template'] = 'ephemeral:' + urlsafe_b64encode(self.extraction_ephemeral_template.encode('utf-8')).decode('utf-8')
 
         if self.extraction_prompt:
             params['extraction_prompt'] = quote_plus(self.extraction_prompt)
